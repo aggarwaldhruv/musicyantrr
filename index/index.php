918282028c1401cc0062afb154a4e3cb65e6ee6f
aggarwaldhruv <aggarwaldhruv1993@gmail.com>
2015-02-28 23:28:33 +0530

    Create index.php

---
+++ index/index.php
@@ -0,0 +1,44 @@
+<html>
+<head></head>
+<body bgcolor='black'>
+<script>setTimeout(function() {
+    window.location.assign('../homepage/index.php');
+}, 2000);</script>
+<style>
+
+ h1#neon-tubing {
+ 	margin: auto 0;
+padding: 6rem; text-align: center;
+color: rgba(255,200,200,0.8);
+font-family: Neon80s, sans-serif;
+font-size: 10rem; letter-spacing: 2rem;
+
+background-size: cover;
+text-shadow: 0 0 10px #fff,0 0 20px #fff,0 0 30px #fff,0 0 40px #1eeddb,0 0 70px #1eeddb,0 0 80px #1eeddb,0 0 100px #1eeddb,0 0 150px #1eeddb;
+h1#neon-tubing { animation: neonspark 1s 3s forwards; }
+h1#neon-tubing span { animation: neonspark 4s 3s infinite; }
+}
+
+} 
+
+body
+ {
+
+}
+
+</style>
+<div>
+<div >
+<center>
+<img src="images.jpg" alt="Smiley face" height="200" width="700">
+</center>
+</div>
+<div>
+<h1 id="neon-tubing">MUS<span>I</span><span>C</span>
+<span>Y</span><span>A</span><span>N</span><span>T</span><span>R</span><span>R</span></h1>
+</div>
+</div>
+
+</body>
+</html>
+
