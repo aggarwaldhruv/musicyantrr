6d231f45198c4423148413ef1d1643c2d4eca8b3
aggarwaldhruv <aggarwaldhruv1993@gmail.com>
2015-02-28 23:39:09 +0530

    Update and rename cart to contact/index.php

---
+++ contact/index.php
@@ -0,0 +1,35 @@
+<?php 
+include '../layout/index.php';
+?>
+<div class="main_pro" style="padding:20px;">
+<table style="display:inline-flex;">
+	
+<tr>
+	<h2>Call us: </h2><br>
+
+		We're available 24 hours a day.<br>
+		(0124) 6150000 or 1800 208 9898<br>
+<br><br>
+		Need assistance in buying?<br>
+		Call our product experts 1800 1080 1800<br>
+				
+	</tr><tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</tr>
+	<tr>
+		<h2>Email us:</h2><br>
+
+		cs@musicyantrr.com <br>
+	</tr>
+	<tr>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</tr>
+	<tr><h2>Mail us:</h2><br>
+
+		Musicyantrr Private Limited,<br>
+		Ozone Manay Tech Park,<br>
+		#56/78 & 55/09, 3rd Floor,<br>
+		Garvebhavipalya, Hosur Road,<br>
+		Karnataka, India.<br>
+</tr>
+</table>
+<br><br>
+</div>	
+<?php 
+include '../footer.php'; ?>
